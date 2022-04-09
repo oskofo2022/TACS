4886c7be0519d094ed73f7c2fb106a41d7907e0d
patricio.filice@gmail.com <patricio.filice@gmail.com>
2022-04-08 21:12:18 -0300

    Updated URLs Hierarchies.

--- src/main/java/constants/UriConstants.java
+++ src/main/java/constants/UriConstants.java
@@ -6,19 +6,19 @@ public class UriConstants {
         public static final String URL = "users";
         public static final String ID = "/{userId}";
 
+        public static class Myself {
+            public static final String URL = Users.URL + "myself";
+            public static class Inscriptions {
+                public static final String URL = Myself.URL + "/inscriptions";
+                public static class Games {
+                    public static final String URL = Inscriptions.URL + "/games";
+                }
+            }
+        }
+
         public static class Tournaments {
             private static final String URL = Users.URL + "/tournaments";
             public static final String ID = "/{tournamentId}";
-        }
-    }
-
-    public static class Myself {
-        public static final String URL = "myself";
-        public static class Inscriptions {
-            public static final String URL = Myself.URL + "/inscriptions";
-            public static class Games {
-                public static final String URL = Inscriptions.URL + "/games";
-            }
         }
     }
 }
