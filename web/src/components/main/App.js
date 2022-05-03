35c60e156e1b26be9d02fd725bf29154291488cc
Federico Larrea <fedeml.95@gmail.com>
2022-05-03 10:59:41 -0300

    Added saving session in sessionStorage.

--- web/src/components/main/App.js
+++ web/src/components/main/App.js
@@ -17,20 +17,32 @@ function App() {
         setActiveTab(tab);
     }
 
-    const signin = (name=null) => {
+    const signin = (name = null) => {
         setAuthenticated(true);
         setName(name);
+        sessionStorage.setItem('userData',
+            JSON.stringify({name})
+        );
     }
 
     const signout = () => {
         setAuthenticated(false);
         setName(defaultName);
+        sessionStorage.removeItem('userData');
     }
+
+    React.useEffect(() => {
+        const storedData = JSON.parse(sessionStorage.getItem('userData'));
+        if (storedData && storedData.name)
+            signin(storedData.name);
+
+    });
 
     return (
         <main>
             <Router>
-                <AuthContext.Provider value={{authenticated: authenticated, signin: signin, signout: signout, name: name}}>
+                <AuthContext.Provider
+                    value={{authenticated: authenticated, signin: signin, signout: signout, name: name}}>
                     <Header pages={pages} handleTab={handleTab}/>
                     <Home activeTab={activeTab}/>
                     <Footer/>
