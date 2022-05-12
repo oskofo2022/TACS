e0b6e3ca7d09b2b2bee260c9c7a9f338dfa04cda
patricio.filice@gmail.com <patricio.filice@gmail.com>
2022-05-11 21:25:13 -0300

    Transferred environment mutable values to application properties

--- src/main/java/constants/ApplicationProperties.java
+++ src/main/java/constants/ApplicationProperties.java
@@ -18,6 +18,20 @@ public class ApplicationProperties {
                     public static final String PATTERN = Origin.NAME + SEPARATOR + "pattern" + BIND_END_TOKEN;
                 }
             }
+
+            public static class Configuration {
+                private static final String NAME = Cors.NAME + SEPARATOR + "configuration";
+
+                public static class Allowed {
+                    private static final String NAME = Configuration.NAME + SEPARATOR + "allowed";
+
+                    public static class Arguments {
+                        public static final String ORIGINS = Allowed.NAME + SEPARATOR + "origins" + BIND_END_TOKEN;
+                        public static final String HEADERS = Allowed.NAME + SEPARATOR + "headers" + BIND_END_TOKEN;
+                        public static final String METHODS = Allowed.NAME + SEPARATOR + "methods" + BIND_END_TOKEN;
+                    }
+                }
+            }
         }
 
         public static class  Jwt {
@@ -35,6 +49,7 @@ public class ApplicationProperties {
                 public static final String EXPIRATION = Cookie.NAME + SEPARATOR + "expiration" + BIND_END_TOKEN;
                 public static final String NAME = Cookie.NAME + SEPARATOR + "name" + BIND_END_TOKEN;
                 public static final String PATH = Cookie.NAME + SEPARATOR + "path" + BIND_END_TOKEN;
+                public static final String SAME_SITE = Cookie.NAME + SEPARATOR + "same-site" + BIND_END_TOKEN;
             }
         }
 
@@ -44,6 +59,34 @@ public class ApplicationProperties {
                 public static final String SECRET = Hash.NAME + SEPARATOR + "secret" + BIND_END_TOKEN;
                 public static final String ITERATIONS = Hash.NAME + SEPARATOR + "iterations" + BIND_END_TOKEN;
                 public static final String WIDTH = Hash.NAME + SEPARATOR + "width" + BIND_END_TOKEN;
+            }
+        }
+
+        public static class Integrations {
+            private static final String NAME = Wordle.NAME + SEPARATOR + "integrations";
+            public static class Dictionaries {
+                private static final String NAME = Integrations.NAME + SEPARATOR + "dictionaries";
+
+                public static class English {
+                    private static final String NAME = Dictionaries.NAME + SEPARATOR + "english";
+                    public static class Arguments {
+                        public static final String URL = English.NAME + SEPARATOR + "url" + BIND_END_TOKEN;
+                    }
+                }
+
+                public static class Spanish {
+                    private static final String NAME = Dictionaries.NAME + SEPARATOR + "spanish";
+                    public static class Arguments {
+                        public static final String URL = Spanish.NAME + SEPARATOR + "url" + BIND_END_TOKEN;
+                    }
+                    public static class App {
+                        private static final String NAME = Spanish.NAME + SEPARATOR + "app";
+                        public static class Arguments {
+                            public static final String ID = App.NAME + SEPARATOR + "id" + BIND_END_TOKEN;
+                            public static final String KEY = App.NAME + SEPARATOR + "key" + BIND_END_TOKEN;
+                        }
+                    }
+                }
             }
         }
     }
