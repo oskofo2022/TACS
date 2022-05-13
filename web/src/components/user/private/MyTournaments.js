597899e5df025a129e5b65dab36a43655f2e21b4
Federico Larrea <fedeml.95@gmail.com>
2022-05-13 19:53:24 -0300

    Added my tournaments request

--- web/src/components/user/private/MyTournaments.js
+++ web/src/components/user/private/MyTournaments.js
@@ -1,7 +1,101 @@
 import React from "react";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import {Navigate} from 'react-router-dom';
+import {QueryParams} from '../../../httpUtils/QueryParams'
+import {InscriptionsRequest} from "../../../request/InscriptionsRequest";
+import {TournamentsResponse} from "../../../response/TournamentsResponse";
 
-const MyTournaments = () => (
-    <p> My Tournaments</p>
-);
+const MyTournaments = () => {
+    const [data, setData] = React.useState({
+        loading: true,
+        rows: [],
+        totalRows: 0,
+        pageSize: 2,
+        page: 1,
+        rowsPerPageOptions: [2, 5, 10, 20],
+        sortBy: 'id',
+        sortOrder:'ASCENDING',
+    });
+
+    function createData(
+        id: number,
+        name: string,
+        language: string,
+        beginDate: string,
+        endDate: string,
+        state: string,
+    ) {
+        return { id, name, language, beginDate, endDate, state };
+    }
+
+    const updateData = (k, v) => setData((prev) => ({ ...prev, [k]: v }));
+
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 70, sortable: false, },
+        { field: 'name', headerName: 'Name', width: 130, sortable: false, },
+        { field: 'language', headerName: 'Language', width: 130, sortable: false, },
+        { field: 'beginDate', headerName: 'Begin date', width: 130, sortable: false, },
+        { field: 'endDate', headerName: 'End date', width: 130, sortable: false, },
+        { field: 'state', headerName: 'State', width: 130, sortable: false, },
+    ];
+
+    const b = () => {
+        return (<Navigate to={'/logout'}/>);
+    };
+
+    const a = () => (
+        <div style={{ height: '60%', width: '100%' }}>
+            <DataGrid
+                density="compact"
+                autoHeight
+                pagination
+                loading={data.loading}
+                rows={data.rows}
+                columns={columns}
+                page={data.page-1}
+                pageSize={data.pageSize}
+                rowsPerPageOptions={data.rowsPerPageOptions}
+                rowCount={data.totalRows}
+                paginationMode='server'
+                onPageChange={ (p, d) => {updateData("page", p + 1);} }
+                onPageSizeChange={ (ps, d) => {updateData("page", 1); updateData("pageSize", ps);} }
+            />
+        </div>
+    );
+    const handleGetInscriptions = async (inscriptionsRequest): Promise<TournamentsResponse> =>  {
+        updateData("loading", true);
+        return await inscriptionsRequest.fetchAsPaged();
+    }
+
+    React.useEffect(() => {
+
+
+        const inscriptionsRequest = InscriptionsRequest.from(
+            new QueryParams({
+                page: data.page,
+                pageSize: data.pageSize,
+                sortBy: data.sortBy,
+                sortOrder: data.sortOrder
+            })
+        )
+
+        const response = handleGetInscriptions(inscriptionsRequest);
+        response.then( r => {
+            const rows = r.pageItems;
+            const totalRows = r.totalCount;
+            updateData("totalRows", totalRows);
+            updateData("rows", rows);
+            updateData("loading", false);
+        });
+
+    }, [data.page, data.pageSize]);
+
+
+
+    const [template, setTemplate] = React.useState(a());
+
+    // return (<p> My Tournaments</p>)
+    return (<React.Fragment>{template}</React.Fragment>);
+};
 
 export default  MyTournaments;
