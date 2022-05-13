c1614131cf8a766c41731caa623f7d5c8cd6460f
Federico Larrea <fedeml.95@gmail.com>
2022-05-12 23:54:48 -0300

    Added dictionary form validations

--- web/src/components/user/public/Dictionaries.js
+++ web/src/components/user/public/Dictionaries.js
@@ -6,14 +6,27 @@ import Typography from "@mui/material/Typography";
 import SearchIcon from '@mui/icons-material/Search';
 import {LanguagesConstants} from "../../../constants/LanguagesConstants";
 
+const _incorrect_word_msg = 'Sólo se admiten letras y espacios';
+const _empty_word_msg = 'Debe ingresar una palabra.';
+const _incorrect_language_msg = 'Debe seleccionar un idioma';
+
 const Dictionaries = () => {
     const noMeanings=[];
-    //TODO: input validation.
+
     const [language, setLanguage] = React.useState('');
+    const [languageValid, setLanguageValid] = React.useState(true);
+    const [languageHelper, setLanguageHelper] = React.useState('');
+
     const [word, setWord] = React.useState('');
+    const [wordValid, setWordValid] = React.useState(true);
+    const [wordHelper, setWordHelper] = React.useState('');
+
     const [meanings, setmeanings] = React.useState(noMeanings);
     const [wordTitle, setWordTitle] = React.useState('');
     const [pathParam, setPathParam] = React.useState({name:'word', value: word});
+    const [notFound, setNotFound] = React.useState(false);
+    const [notFoundedWord, setNotFoundedWord] = React.useState('');
+
 
     const updateWordParam = (_word) =>
         setPathParam({
@@ -21,28 +34,64 @@ const Dictionaries = () => {
             value: _word.toLowerCase()
         });
 
-
-    const handleLanguageOnChange = (event: SelectChangeEvent) => setLanguage(event.target.value);
-
     const capitalizeFirstLetter = (string) => (string.charAt(0).toUpperCase() + string.slice(1).toLowerCase());
 
+    const validateLanguage = (value) => {
+        const isValid = !!LanguagesConstants[value];
+        setLanguageValid(isValid);
+        (!isValid) ? setLanguageHelper(_incorrect_language_msg) : setLanguageHelper('');
+        return isValid;
+    }
+
+    const validateWord = (value) => {
+        const empty_re = new RegExp("^$");
+        const correct_re = new RegExp("^[a-z A-Z]+$");
+        const isCorrect = correct_re.test(value);
+        const isEmpty = empty_re.test(value);
+        const isValid = (isCorrect && !isEmpty);
+
+        setWordHelper('');
+        if(!isCorrect) setWordHelper(_incorrect_word_msg);
+        if(isEmpty) setWordHelper(_empty_word_msg);
+
+        setWordValid(isValid);
+        return isValid;
+    }
+
+    const allValidations = [{f:validateLanguage, v:language}, {f:validateWord, v:word}];
+
+    const handleLanguageOnChange = (e: SelectChangeEvent) => {
+        const value = e.target.value;
+        validateLanguage(value);
+        setLanguage(value);
+    };
+
     const handleWordOnChange = (e) => {
-        setWord(e.target.value);
-        updateWordParam(e.target.value);
+        const value = e.target.value;
+        validateWord(value);
+        setWord(value);
+        updateWordParam(value);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const isValid = allValidations.reduce((prev, validation) => validation.f(validation.v) && prev, true)
+        if(!isValid) return;
+
         const dictionaryRequest = LanguagesConstants[language].getRequest(pathParam);
         const responseJson = await dictionaryRequest.fetchAsJSON();
 
         //TODO: response validation. Show some error message if server fails.
         if (dictionaryRequest.response.status === 200 && responseJson["meanings"].length > 0) {
+            setNotFound(false);
             setmeanings(responseJson["meanings"]);
             setWordTitle(capitalizeFirstLetter(word));
         } else {
             setmeanings(noMeanings);
             setWordTitle('');
+            setNotFound(true);
+            setNotFoundedWord(word);
         }
     }
 
@@ -60,8 +109,10 @@ const Dictionaries = () => {
                     id="word"
                     label="Search definition"
                     variant="outlined"
-                    helperText="Please insert a word"
+                    helperText={wordHelper}
                     onChange={handleWordOnChange}
+                    error={!wordValid}
+                    required={true}
                 />
                 <TextField
                     id="outlined-select-language"
@@ -69,7 +120,9 @@ const Dictionaries = () => {
                     label="Language"
                     value={language}
                     onChange={handleLanguageOnChange}
-                    helperText="Please select the language"
+                    helperText={languageHelper}
+                    error={!languageValid}
+                    required={true}
                 >
                     {Object.entries(LanguagesConstants).map(([key, value])=>(
                         <MenuItem key={key} value={key}>{value.label}</MenuItem>
@@ -88,11 +141,19 @@ const Dictionaries = () => {
                     Search
                 </Button>
             </Box>
+            {!!notFound && <Typography
+                variant="h5"
+                noWrap
+                component="div"
+                sx={{flexGrow: 1, color: 'black', marginTop: '8%', marginLeft: '5%'}}
+            >
+                No se encontraron resultados para la palabra {notFoundedWord}
+            </Typography>}
             <Typography
                 variant="h3"
                 noWrap
                 component="div"
-                sx={{flexGrow: 1, color: 'black'}}
+                sx={{flexGrow: 1, color: 'black',  marginTop: '5%'}}
             >
                 {wordTitle}
             </Typography>
